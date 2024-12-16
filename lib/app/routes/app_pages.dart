1bb203eaa8ad2038a0ba05bdcbd79533562f1ad6
JpFontinele <jfontinele@id.uff.br>
2024-12-16 09:52:18 -0300

    Criado a interface das noticias
    Ainda está de forma a apenas mostrar uma ideia do que será realizado, ainda contem erros

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -6,11 +6,15 @@ import 'package:redescomunicacionais/app/routes/app_routes.dart';
 import 'package:redescomunicacionais/app/ui/device/home_page.dart';
 import 'package:redescomunicacionais/app/ui/device/initial_page.dart';
 import 'package:redescomunicacionais/app/ui/device/login_page.dart';
+import 'package:redescomunicacionais/app/data/components/news.dart';
+import 'package:redescomunicacionais/app/ui/device/news_page.dart';
 
 class AppPages {
   static final routes = [
     GetPage(name: Routes.INITIAL, page: () => InitialPage(),),
     GetPage(name: Routes.LOGIN, page: () => LoginPage(), binding: LoginBinding()),
-    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding())
+    GetPage(name: Routes.HOME, page: () => HomePage(), binding: HomeBinding()),
+    GetPage(name: Routes.NEWS, page: () => News(),),
+    GetPage(name: Routes.NEWSPAGE, page: ()=> NewsPage(),),
   ];
 }
---
+++ lib/app/ui/device/news_page.dart
@@ -0,0 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:redescomunicacionais/app/routes/app_routes.dart';
+
+class NewsPage extends StatefulWidget {
+  const NewsPage({super.key});
+
+  @override
+  State<NewsPage> createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  final db = FirebaseFirestore.instance;
+  final storage = FirebaseStorage.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text("Noticia completa"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.reset_tv_rounded,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              print("VOltando");
+              Get.back();
+            }
+            ,),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              ShaderMask(
+                shaderCallback: (rect) {
+                  return const LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.white, Colors.black],
+                  ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                },
+                /*child: Image.memory(
+                        movie["image"],
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ),*/
+                child: Image.network(
+                  "https://cmssalto.rj.gov.br/wp-content/uploads/2020/10/FOTO-SAO-SEBASTIAO-DO-ALTO.jpg", // Substitua com a URL da imagem desejada
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Expanded(
+                child: Text(
+                  "Eleições 2024: Claudiane Pietrani, do SOLIDARIEDADE, é eleita prefeita de São Sebastião do Alto no 1º turno",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22),
+                ),
+              ),
+            ],
+          ),
+          const Text("Fontinele sinistro",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w100,
+                  fontSize: 12)),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Claudiane Pietrani, do SOLIDARIEDADE, foi eleita prefeita de São Sebastião do Alto (RJ) neste domingo (6), com 3.474 votos, correspondendo a 51,22% dos votos válidos. A eleição teve 7.041 votos totais, incluindo 68 votos brancos (0,97%) e 191 nulos (2,71%). Aledio Rezende, do PL, obteve 3.308 votos, 48,78% dos votos válidos. Esses dados foram divulgados pelo Tribunal Superior Eleitoral (TSE).",
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
