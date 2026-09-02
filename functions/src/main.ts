88917313c3af1823759365e9403ed0a871354f5d
Carina Lanchine <92098147+carinalanchine@users.noreply.github.com>
2026-09-02 11:06:01 -0300

    fix: changed deploy region

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,8 +1,12 @@
 import { setGlobalOptions } from "firebase-functions/v2/options";
 import * as admin from "firebase-admin";
 
-setGlobalOptions({ maxInstances: 10 });
 admin.initializeApp();
+
+setGlobalOptions({
+  region: 'southamerica-east1',
+  maxInstances: 10
+});
 
 export * from "./controller/health-check";
 export * from "./controller/public-key.controller";
